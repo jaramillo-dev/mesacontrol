ddddd7a25ee5bcf4dc18990aa509d741bce2bcae
Juan Jaramillo <195936872+jaramillo-dev@users.noreply.github.com>
2026-06-03 23:03:31 -0600

    chore: initial commit

---
+++ src/main/java/com/biometec/mesacontrol/MesacontrolApplication.java
@@ -0,0 +1,13 @@
+package com.biometec.mesacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesacontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesacontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biometec/mesacontrol/MesacontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biometec.mesacontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesacontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
